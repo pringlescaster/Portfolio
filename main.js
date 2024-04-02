ca4c6022fcea590cf88610721d9c8ea47ceeef29
unknown <pringlescaster@gmail.com>
2024-04-02 10:41:23 +0100

    Hero Section

---
+++ main.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    var sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    var sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
